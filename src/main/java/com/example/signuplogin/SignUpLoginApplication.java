027e30825e44ca2d78fc34b7c3c2bc6aa26ff954
soyeonkim0227 <soyeonkim0227@naver.com>
2022-05-26 23:33:55 +0900

    Create SpringBoot

---
+++ src/main/java/com/example/signuplogin/SignUpLoginApplication.java
@@ -0,0 +1,13 @@
+package com.example.signuplogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignUpLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SignUpLoginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/signuplogin/SignUpLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.signuplogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignUpLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
